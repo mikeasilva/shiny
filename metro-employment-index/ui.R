61d6cc6b525b210dc5a091063bb1b3392ad54ec8
Mike Silva <mike.a.silva@gmail.com>
2014-12-11 15:09:09 -0500

    Updated BEA Data

--- metro-employment-index/ui.R
+++ metro-employment-index/ui.R
@@ -1,15 +1,19 @@
 shinyUI(
   fluidPage(
-    title = "Metro Employment Levels",
+    title = 'Metro Employment Levels',
+    responsive = TRUE,
+  
     fluidRow(
-      column(width = 12, h2("Metro Employment Levels"))
+      column(width = 12, 
+             h2('Metro Employment Levels'), 
+             p('The following presents seasonally adjusted employment levels indexed to two possible points in time (baselines).  You can highlight one metro area\'s data.  For more information about how the index was created visit ', a('http://rpubs.com/mikesilva/metro-employment-index'), '.'))
     ),
     fluidRow(
       column(width = 6, selectInput('dataset', 'Baseline', c('Jan 2000 = 100','Nov 2007 = 100'))),
       column(width = 6, selectInput('highlight', 'Highlight', c('None',msa.names)))
     ),
     fluidRow(
-      column(width = 12, plotOutput('plot'))
+      column(width = 12, align='center', plotOutput('plot'))
     )
   )
 )
